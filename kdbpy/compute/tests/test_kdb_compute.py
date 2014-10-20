40e5fdc48571863ff08ed38d68764fe4997facf2
Phillip Cloud <cpcloud@gmail.com>
2014-10-20 10:03:25 -0400

    Tests

--- kdbpy/compute/tests/test_kdb_compute.py
+++ kdbpy/compute/tests/test_kdb_compute.py
@@ -1,9 +1,8 @@
-import getpass
 import pytest
 
 import numpy as np
-from blaze import Symbol, compute
-from kdbpy import into, resource, Q
+from blaze import Symbol, compute, by
+from kdbpy import Q
 
 
 @pytest.fixture(scope='module')
@@ -23,23 +22,70 @@ def t():
 
 @pytest.fixture(scope='module')
 def q(t):
-    return Q(t=t._name, q=[])
+    return Q(t=t, q=[])
 
 
 def test_projection(t, q):
-    expr = t[['a', 'b']]
+    expr = t[['id', 'amount']]
     result = compute(expr, q)
-    assert result == 'select a, b from t'
+    assert result == '(?; `t; 0b; (`id; `amount)!(`id; `amount))'
+
+
+def test_single_projection(t, q):
+    expr = t[['id']]
+    result = compute(expr, q)
+    assert result == '(?; `t; 0b; (enlist[`id])!(enlist[`id]))'
 
 
 def test_selection(t, q):
-    expr = t[t.a > 1.0]
+    expr = t[t.id == 1]
     result = compute(expr, q)
-    assert result == 'select from t where t.a > 1.0'
+    assert result == '(?; `t; (enlist[(enlist[(=; `t.id; 1)])]); 0b; ())'
+
+
+def test_broadcast(t, q):
+    expr = t.id + 1
+    result = compute(expr, q)
+    assert result == '(+; `t.id; 1)'
 
 
 def test_complex_selection_projection(t, q):
-    expr = t[(t.id + 1 - 2* t.id * t.id + t.amount) > t.id - 3][['id',
-                                                                 'amount']]
+    expr = t[t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3][['id', 'amount']]
     result = compute(expr, q)
-    assert result == 'select id, amount from t where t.id + 1 - 2 * t.id * t.id + t.amount > 1.id - 3'
+    import ipdb; ipdb.set_trace()
+    e = 'enlist[(enlist[(>; (+; `t.id; (-; 1; (*; 2; (+; (xexp; `t.id; 2); `t.amount)))); (-; `t.id; 3))])]'
+    expected = '(?; `t; %s; 0b; (`id; `amount)!(`id; `amount))' % e
+    assert result == expected
+
+
+def test_unary_op(t, q):
+    expr = -t.amount
+    result = compute(expr, q)
+    assert result == '(-:; `t.amount)'
+
+
+def test_string(t, q):
+    expr = t.name == 'Alice'
+    result = compute(expr, q)
+    expected = '(=; `t.name; `Alice)'
+    assert result == expected
+
+
+@pytest.mark.xfail
+def test_simple_by(t, q):
+    expr = by(t.name, t.amount.sum())
+    result = compute(expr, q)
+    assert result == False
+    assert result == 'select (sum (t.amount)) by (t.name) from (t)'
+
+
+def test_field(t, q):
+    expr = t.name
+    result = compute(expr, q)
+    assert result == '`t.name'
+
+
+def test_sum(t, q):
+    expr = t.amount.sum()
+    result = compute(expr, q)
+    assert result == "(sum; `t.amount)"
