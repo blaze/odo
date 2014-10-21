ed20b2759c180dffeeb84525e4a121b24780125b
Phillip Cloud <cpcloud@gmail.com>
2014-10-20 21:34:09 -0400

    Convert tests to compare against QLists instead of strings

--- kdbpy/compute/tests/test_kdb_compute.py
+++ kdbpy/compute/tests/test_kdb_compute.py
@@ -1,8 +1,9 @@
+from __future__ import print_function, division, absolute_import
 import pytest
 
 import numpy as np
 from blaze import Symbol, compute, by
-from kdbpy import Q
+from kdbpy import Q, QString, QList, QDict, QSymbol, QBool
 
 
 @pytest.fixture(scope='module')
@@ -25,16 +26,51 @@ def q(t):
     return Q(t=t, q=[])
 
 
+def test_qlist():
+    x = QList(QSymbol('a'), 1, 2)
+    assert repr(x) == '(`a; 1; 2)'
+
+    x = QList(QSymbol('a sym'), QList(QSymbol('a')), 3)
+    assert repr(x) == '(`$"a sym"; (enlist[`a]); 3)'
+
+
+def test_qdict():
+    x = QDict([(QSymbol('a'), 1), (QSymbol('b'), 2)])
+    assert repr(x) == '(`a; `b)!(1; 2)'
+
+
+def test_qsymbol():
+    s = QSymbol('a')
+    assert repr(s) == '`a'
+
+    s = QSymbol('a symbol')
+    assert repr(s) == '`$"a symbol"'
+
+
+def test_qstring():
+    s = QString('s')
+    assert repr(s) == '"s"'
+
+    s = QString('"s"')
+    assert repr(s) == '"\"s\""'
+
+
 def test_projection(t, q):
     expr = t[['id', 'amount']]
     result = compute(expr, q)
-    assert result == '(?; `t; 0b; (`id; `amount)!(`id; `amount))'
+    fields = [QSymbol('id'), QSymbol('amount')]
+    expected = QList('?', QSymbol('t'), QList(), QBool(False),
+                     QDict(list(zip(fields, fields))))
+    import ipdb; ipdb.set_trace()
+    assert result == expected
 
 
 def test_single_projection(t, q):
     expr = t[['id']]
     result = compute(expr, q)
-    assert result == '(?; `t; 0b; (enlist[`id])!(enlist[`id]))'
+    assert result == QList('?', QSymbol('t'), QList(),
+                           QBool(False), QDict([(QSymbol('id'),),
+                                                (QSymbol('id'),)]))
 
 
 def test_selection(t, q):
@@ -49,13 +85,33 @@ def test_broadcast(t, q):
     assert result == '(+; `t.id; 1)'
 
 
-def test_complex_selection_projection(t, q):
-    expr = t[t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3][['id', 'amount']]
+def test_complex_arith(t, q):
+    expr = t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3
     result = compute(expr, q)
-    import ipdb; ipdb.set_trace()
-    e = 'enlist[(enlist[(>; (+; `t.id; (-; 1; (*; 2; (+; (xexp; `t.id; 2); `t.amount)))); (-; `t.id; 3))])]'
-    expected = '(?; `t; %s; 0b; (`id; `amount)!(`id; `amount))' % e
+    big = '(+; (-; (+; `t.id; 1); (*; 2; (xexp; `t.id; 2))); `t.amount)'
+    expected = '(>; %s; (-; `t.id; 3))' % big
     assert result == expected
+
+
+def test_complex_selection(t, q):
+    expr = t[t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3]
+    result = compute(expr, q)
+    big = '(+; (-; (+; `t.id; 1); (*; 2; (xexp; `t.id; 2))); `t.amount)'
+    where = '(>; %s; (-; `t.id; 3))' % big
+    assert result == '(?; `t; (enlist[(enlist[%s])]); 0b; ())' % where
+
+
+def test_complex_selection_projection(t, q):
+    expr = t[t.id ** 2 + t.amount > t.id - 3][['id', 'amount']]
+    result = compute(expr, q)
+    child = QList()
+    constraint = QList()
+    by = QBool(False)
+    fields = QSymbol('id'), QSymbol('amount')
+    expected = QList('?', child, constraint, by,
+                     QDict(list(zip(fields, fields))))
+    assert result == expected
+
 
 
 def test_unary_op(t, q):
@@ -64,10 +120,10 @@ def test_unary_op(t, q):
     assert result == '(-:; `t.amount)'
 
 
-def test_string(t, q):
+def test_string_compare(t, q):
     expr = t.name == 'Alice'
     result = compute(expr, q)
-    expected = '(=; `t.name; `Alice)'
+    expected = '(=; `t.name; "Alice")'
     assert result == expected
 
 
