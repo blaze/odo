99e08d0c16539d2c6c21bf9d5095d4a9656c38cc
Phillip Cloud <cpcloud@gmail.com>
2014-10-21 16:57:19 -0400

    Namespace compute

--- kdbpy/__init__.py
+++ kdbpy/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 from multipledispatch import halt_ordering, restart_ordering
 halt_ordering()
-from .compute import *
+from . import compute
 restart_ordering()
 
 import logging
