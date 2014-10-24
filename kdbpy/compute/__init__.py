e7f562ca6e46936d95f2be59b8a9641e673b7ff3
Phillip Cloud <cpcloud@gmail.com>
2014-10-24 11:42:41 -0400

    Export tables in toplevel API

--- kdbpy/compute/__init__.py
+++ kdbpy/compute/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from multipledispatch import halt_ordering, restart_ordering
 halt_ordering()
-from .kdb import QTable
+from .kdb import QTable, tables
 restart_ordering()
 from . import q
