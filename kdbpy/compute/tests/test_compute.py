48ca6fc45b9dd2dde2312f1f9ab6d1f528d277b8
Phillip Cloud <cpcloud@gmail.com>
2014-10-25 12:22:45 -0400

    Move filename

---
+++ kdbpy/compute/tests/test_compute.py
@@ -0,0 +1,172 @@
+from __future__ import print_function, division, absolute_import
+
+import pytest
+
+import pandas as pd
+import pandas.util.testing as tm
+import blaze as bz
+from blaze import compute, into, by, discover, dshape
+from kdbpy.compute import tables
+from kdbpy.compute.q import List, Symbol, Dict, String
+
+
+def test_qlist():
+    x = List(Symbol('a'), 1, 2)
+    assert repr(x) == '(`a; 1; 2)'
+
+    x = List(Symbol('a sym'), List(Symbol('a')), 3)
+    assert repr(x) == '(`$"a sym"; (enlist[`a]); 3)'
+
+
+def test_qdict():
+    x = Dict([(Symbol('a'), 1), (Symbol('b'), 2)])
+    assert repr(x) == '(`a; `b)!(1; 2)'
+
+
+def test_qsymbol():
+    s = Symbol('a')
+    assert repr(s) == '`a'
+
+    s = Symbol('a symbol')
+    assert repr(s) == '`$"a symbol"'
+
+
+def test_qstring():
+    s = String('s')
+    assert repr(s) == '"s"'
+
+    s = String('"s"')
+    assert repr(s) == '"\"s\""'
+
+
+def test_projection(t, q, df):
+    expr = t[['id', 'amount']]
+    expected = compute(expr, df)
+    result = into(pd.DataFrame, compute(expr, q))
+    tm.assert_frame_equal(result, expected)
+
+
+def test_single_projection(t, q, df):
+    expr = t[['id']]
+    result = into(pd.DataFrame, compute(expr, q))
+    tm.assert_frame_equal(result, compute(expr, df))
+
+
+def test_selection(t, q, df):
+    expr = t[t.id == 1]
+    result = into(pd.DataFrame, compute(expr, q))
+    expected = compute(expr, df)
+    tm.assert_frame_equal(result, expected)
+
+
+def test_broadcast(t, q, df):
+    expr = t.id + 1
+    result = into(pd.Series, compute(expr, q))
+    expected = compute(expr, df)
+    tm.assert_series_equal(result, expected)
+
+
+def test_complex_broadcast(t, q, df):
+    expr = t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3
+    result = into(pd.Series, compute(expr, q))
+    expected = compute(expr, df)
+    tm.assert_series_equal(result, expected)
+
+
+def test_complex_selection(t, q, df):
+    expr = t[t.id + 1 - 2 * t.id ** 2 + t.amount > t.id - 3]
+
+    # q doesn't know anything pandas indexes
+    result = into(pd.DataFrame, compute(expr, q))
+
+    # but blaze preserves them
+    expected = compute(expr, df).reset_index(drop=True)
+    tm.assert_frame_equal(result, expected)
+
+
+def test_complex_selection_projection(t, q, df):
+    expr = t[t.id ** 2 + t.amount > t.id - 3][['id', 'amount']]
+
+    # q doesn't know anything pandas indexes
+    result = into(pd.DataFrame, compute(expr, q))
+
+    # but blaze preserves them
+    expected = compute(expr, df).reset_index(drop=True)
+    tm.assert_frame_equal(result, expected)
+
+
+def test_unary_op(t, q, df):
+    expr = -t.amount
+    result = into(pd.Series, compute(expr, q))
+    expected = compute(expr, df)
+    tm.assert_series_equal(result, expected)
+
+
+def test_string_compare(t, q, df):
+    expr = t.name == 'Alice'
+    result = into(pd.Series, compute(expr, q))
+    expected = compute(expr, df)
+    tm.assert_series_equal(result, expected)
+
+
+def test_simple_by(t, q, df):
+    # q) select name, amount_sum: sum amount from t
+    expr = by(t.name, t.amount.sum())
+    qresult = compute(expr, q)
+    result = into(pd.DataFrame, qresult).reset_index()
+
+    # q fills NaN reducers with 0
+    expected = compute(expr, df).fillna(0)
+    tm.assert_frame_equal(result, expected)
+
+    result = into(pd.DataFrame, compute(expr, q))
+    expected = compute(expr, df).set_index('name', drop=True).fillna(0)
+    tm.assert_frame_equal(result, expected)
+
+
+def test_field(t, q, df):
+    expr = t.name
+    result = compute(expr, q)
+    tm.assert_series_equal(pd.Series(result), compute(expr, df))
+
+
+def test_sum(t, q, df):
+    expr = t.amount.sum()
+    result = compute(expr, q)
+    assert result == compute(expr, df)
+
+
+def test_count(t, q, df):
+    expr = t.amount.count()
+    result = compute(expr, q)
+    assert result == len(df)
+
+
+@pytest.mark.xfail(raises=NotImplementedError,
+                   reason='Join not implemented for QTables')
+def test_simple_join(rt, st, rq, sq, rdf, sdf):
+    expr = bz.join(rt, st)
+    result = into(pd.DataFrame, compute(expr, {st: sq, rt: rq}))
+    expected = compute(expr, {st: sdf, rt: rdf})
+    tm.assert_frame_equal(result, expected)
+
+
+def test_sort(t, q, df):
+    expr = t.sort('name')
+    qresult = compute(expr, q)
+    expected = compute(expr, df).reset_index(drop=True)  # q doesn't keep index order
+    result = into(pd.DataFrame, qresult)
+    tm.assert_frame_equal(result, expected)
+
+
+@pytest.mark.xfail(raises=AttributeError,
+                   reason='Issue with Data resource on our side')
+def test_resource(q):
+    data = bz.Data(q, name=q.tablename)
+    result = data.amount + 1
+    assert repr(result)
+
+
+def test_discover(q):
+    assert (str(discover(q)) ==
+            str(dshape('var * {name: string, id: int64, amount: float64}')))
