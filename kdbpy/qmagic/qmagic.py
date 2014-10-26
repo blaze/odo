45eb663c41f462aff92649f7505d222d68c6eefb
Phillip Cloud <cpcloud@gmail.com>
2014-10-25 20:59:38 -0400

    Allow path spec

--- kdbpy/qmagic/qmagic.py
+++ kdbpy/qmagic/qmagic.py
@@ -1,4 +1,4 @@
-# import re
+import os
 from IPython.core.magic_arguments import (argument, magic_arguments,
                                           parse_argstring)
 from IPython.core.magic import (Magics, magics_class, line_magic,
@@ -16,33 +16,32 @@ class QMagic(Magics):
         super(QMagic, self).__init__(shell=shell)
 
     @magic_arguments()
-    @argument('-d', '--data-frame', action='store_true', help='')
+    @argument('-d', '--data-frame', action='store_true',
+              help='Return output as a pandas DataFrame')
+    @argument('-p', '--path', default=os.environ.get('QEXEC'))
     @argument('code', nargs='*')
     @needs_local_scope
     @line_magic
     def q(self, line, local_ns=None):
-        """Runs SQL statement against a database, specified by SQLAlchemy
-        connection string.
-
-        If no database connection has been established, first word
-        should be a SQLAlchemy connection string, or the user@db name
-        of an established connection.
-
+        """ Run a Q program
         Examples::
 
-          %%q
-          t: ([] name: `a`b`c; amount: 1.0 2.0. 3.0; id: 1 + til 3)
-          t
+          %q t: ([] name: `a`b`c; amount: 1.0 2.0. 3.0; id: 1 + til 3); t
 
-          %%q
-          select from t where amount > 1
+          df = %q -d t: ([] name: `a`b; amount: 1 2); select from t where amount > 1
+
+          %q /path/to/q ([] name: `a`b; amount: 3 4)
         """
         args = parse_argstring(self.q, line)
         code = (' '.join(args.code) + '\n').encode('utf8')
 
         def qnative(s):
             import subprocess
-            process = subprocess.Popen(['/Users/pcloud/code/q/m32/q'],
+            if args.path is None:
+                raise ValueError("Please define the QEXEC environment variable "
+                                 "to point to your Q interpreter or path the "
+                                 "--path argument")
+            process = subprocess.Popen([os.path.expanduser(args.path)],
                                        stdin=subprocess.PIPE,
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.STDOUT)
@@ -52,6 +51,7 @@ class QMagic(Magics):
         def qpython(s):
             user_ns = self.shell.user_ns
             user_ns.update(local_ns or {})
+            # TODO: add a connection string here
             return KQ(get_credentials(), start='restart').eval(s, **user_ns)
 
         return qpython(code) if args.data_frame else qnative(code)
