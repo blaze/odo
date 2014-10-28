02acff472824b871d181d97e5f81d4a66df4f42b
Phillip Cloud <cpcloud@gmail.com>
2014-10-28 18:00:56 -0400

    Fix imports for qext/expr

--- kdbpy/compute/__init__.py
+++ kdbpy/compute/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from multipledispatch import halt_ordering, restart_ordering
 halt_ordering()
+from .qext import *
 from .core import *
 restart_ordering()
 from .qtable import QTable, tables
