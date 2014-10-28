44371a8ccc45534d1f16d4b7e891ffae8b649af1
Phillip Cloud <cpcloud@gmail.com>
2014-10-28 15:59:21 -0400

    Fix imports

--- kdbpy/compute/__init__.py
+++ kdbpy/compute/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from multipledispatch import halt_ordering, restart_ordering
 halt_ordering()
-from .kdb import QTable, tables
+from .core import *
 restart_ordering()
+from .qtable import QTable, tables
 from . import q
