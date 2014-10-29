b31a1d097d748de046d2f29749c37e080fddb550
Phillip Cloud <cpcloud@gmail.com>
2014-10-29 11:58:40 -0400

    Test dshape properties and fields for Bar expr

--- kdbpy/compute/tests/test_qext.py
+++ kdbpy/compute/tests/test_qext.py
@@ -1,9 +1,13 @@
 import pandas.util.testing as tm
+from datashape import dshape
 from blaze import compute
 
 
 def test_bar(t, q, df):
     expr = t.amount.bar(5)
+    assert expr.fields == t.amount.fields
+    assert expr.dshape == dshape('var * int64')
+    assert expr.schema == dshape('int64')
     result = compute(expr, q)
     expected = df.amount - df.amount % 5
     tm.assert_series_equal(result, expected)
