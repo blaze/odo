316a72b3f77b4a0ae29e788d521efd18919622c1
Phillip Cloud <cpcloud@gmail.com>
2014-10-30 10:53:25 -0400

    Pass to file so we do not have to print to stdout when testing

--- kdbpy/__init__.py
+++ kdbpy/__init__.py
@@ -16,17 +16,19 @@ logger.setLevel(logging.WARNING)
 __version__ = '0.0.2'
 
 
-def print_versions():
+def print_versions(file=None):
     """Print all the versions of software that kdbpy relies on."""
-    print("-=" * 38)
-    print("kdbpy version: %s" % __version__)
-    print("Python version: %s" % sys.version)
+    print("-=" * 38, file=file)
+    print("kdbpy version: %s" % __version__, file=file)
+    print("Python version: %s" % sys.version, file=file)
     (sysname, nodename, release, version, machine, processor) = platform.uname()
-    print("Platform: %s-%s-%s (%s)" % (sysname, release, machine, version))
+    print("Platform: %s-%s-%s (%s)" % (sysname, release, machine, version),
+          file=file)
     if sysname == "Linux":
-        print("Linux dist: %s" % " ".join(platform.linux_distribution()[:-1]))
+        print("Linux dist: %s" % " ".join(platform.linux_distribution()[:-1]),
+              file=file)
     if not processor:
         processor = "not recognized"
-    print("Processor: %s" % processor)
-    print("Byte-ordering: %s" % sys.byteorder)
-    print("-=" * 38)
+    print("Processor: %s" % processor, file=file)
+    print("Byte-ordering: %s" % sys.byteorder, file=file)
+    print("-=" * 38, file=file)
