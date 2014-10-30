07e3d939cc13934e1bd75ed2e6d4898d2fdc680e
Phillip Cloud <cpcloud@gmail.com>
2014-10-30 16:50:13 -0400

    Notebook fixes

--- kdbpy/qmagic/qmagic.py
+++ kdbpy/qmagic/qmagic.py
@@ -1,60 +1,106 @@
-import os
+import re
+from sqlalchemy.engine.url import make_url
+
 from IPython.core.magic_arguments import (argument, magic_arguments,
                                           parse_argstring)
-from IPython.core.magic import (Magics, magics_class, line_magic,
+from IPython.core.magic import (Magics, magics_class, line_cell_magic,
                                 needs_local_scope)
 from kdbpy.kdb import KQ, get_credentials
 
 
+pattern = re.compile(r'''
+            (?:(?P<name>[\w\+]+)://)?
+            (?:
+                (?P<username>[^:/]*)
+                (?::(?P<password>.*))?
+            @)?
+            (?:
+                (?P<ipv4host>[^/:]+)
+                (?::(?P<port>[^/]*))?
+            )?
+            (?:/(?P<database>.*))?
+            ''', re.X)
+
+
+def get_url(url):
+    gd = pattern.match(url).groupdict()
+    username = gd.get('username')
+    if username is None:
+        raise ValueError('please provide a username')
+    host = gd.get('ipv4host')
+    if host is None:
+        raise ValueError('please provide a host')
+    return get_credentials(username=username,
+                           host=host,
+                           port=gd.get('port'),
+                           password=gd.get('password'))
+
+
 @magics_class
 class QMagic(Magics):
-    """Runs a Q statement on a table, specified by SQLAlchemy connect string.
+    """Runs a Q statement on a table specified by SQLAlchemy connect string.
 
-    Provides the %%q magic."""
+    Provides the %%q and %q magics."""
+
+    _creds = []
 
     def __init__(self, shell):
         super(QMagic, self).__init__(shell=shell)
 
     @magic_arguments()
-    @argument('-d', '--data-frame', action='store_true',
-              help='Return output as a pandas DataFrame')
-    @argument('-p', '--path', default=os.environ.get('QEXEC'))
+    @argument('-d', '--debug', action='store_true')
+    @argument('-c', '--connection-string', default=None,
+              help='KDB connection string')
+    @argument('-r', '--restart', action='store_true',
+              help='If passed, then restart the Q interpreter')
+    @argument('-a', '--assign', default=None)
     @argument('code', nargs='*')
     @needs_local_scope
-    @line_magic
-    def q(self, line, local_ns=None):
-        """ Run a Q program
+    @line_cell_magic
+    def q(self, line, cell='', local_ns=None):
+        """Run a Q program
+
         Examples::
 
-          %q t: ([] name: `a`b`c; amount: 1.0 2.0. 3.0; id: 1 + til 3); t
+          %%q -a result
+          t: ([] name: `a`b`c; amount: 1.0 2.0. 3.0; id: 1 + til 4);
+          x: t.amount + 1
+          x
 
-          df = %q -d t: ([] name: `a`b; amount: 1 2); select from t where amount > 1
-
-          %q /path/to/q ([] name: `a`b; amount: 3 4)
+          df = %q t: ([] name: `a`b; amount: 1 2); select from t where amount > 1
         """
         args = parse_argstring(self.q, line)
-        code = (' '.join(args.code) + '\n').encode('utf8')
+        code = '%s\n%s' % (' '.join(args.code), cell)
+        rx = re.compile(r'\s{2,}')
+        code = code.strip().replace('\n', ' ').replace('\r', '').encode('utf8')
+        code = rx.sub(' ', code)
+        if args.debug:
+            print(code)
+        try:
+            creds = get_url(args.connection_string)
+        except TypeError:
+            pass
+        if self._creds:
+            creds = self._creds[0]
+        else:
+            conn = make_url(args.connection_string)
+            try:
+                creds = get_credentials(port=conn.port, username=conn.username,
+                                        host=conn.host, password=conn.password)
+            except AttributeError:
+                raise ValueError("please provide a connection string")
+            if creds not in self._creds:
+                self._creds.append(creds)
 
-        def qnative(s):
-            import subprocess
-            if args.path is None:
-                raise ValueError("Please define the QEXEC environment variable "
-                                 "to point to your Q interpreter or path the "
-                                 "--path argument")
-            process = subprocess.Popen([os.path.expanduser(args.path)],
-                                       stdin=subprocess.PIPE,
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.STDOUT)
-            stdout, _ = process.communicate(input=s)
-            print(stdout.rstrip('\n'))
-
-        def qpython(s):
-            user_ns = self.shell.user_ns
-            user_ns.update(local_ns or {})
-            # TODO: add a connection string here
-            return KQ(get_credentials(), start='restart').eval(s, **user_ns)
-
-        return qpython(code) if args.data_frame else qnative(code)
+        user_ns = self.shell.user_ns
+        user_ns.update(local_ns or {})
+        result = KQ(creds,
+                    start='restart' if args.restart else True).eval(code,
+                                                                    **user_ns)
+        if args.assign is not None:
+            user_ns[args.assign] = result
+        else:
+            return result
 
 
 def load_ipython_extension(ip):
