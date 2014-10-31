6a96b33f91e40dd8a420d3d8a033da2f7d227a0f
Phillip Cloud <cpcloud@gmail.com>
2014-10-31 10:25:32 -0400

    bar needs fields

--- kdbpy/compute/expr.py
+++ kdbpy/compute/expr.py
@@ -1,22 +1,22 @@
 import datashape
 from datashape.predicates import iscollection, isnumeric
-from blaze.expr import broadcast, Expr
+from blaze.expr import Expr
 from blaze.expr.expressions import schema_method_list, dshape_method_list
 
 
-__all__ = ['Bar']
+__all__ = ['bar']
 
 
-class Bar(Expr):
+class bar(Expr):
     __slots__ = '_child', 'n'
 
     @property
     def dshape(self):
         return datashape.DataShape(datashape.Var(), datashape.int64)
 
-
-def bar(expr, n):
-    return broadcast(Bar, expr, n)
+    @property
+    def fields(self):
+        return self._child.fields
 
 
 schema_method_list.extend([(isnumeric, set([bar]))])
