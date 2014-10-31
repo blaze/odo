7d9decc821de506ee92d6625180fa6670ae6a784
Phillip Cloud <cpcloud@gmail.com>
2014-10-31 10:51:44 -0400

    Change directory before reading

--- kdbpy/qmagic/tests/test_qmagic.py
+++ kdbpy/qmagic/tests/test_qmagic.py
@@ -16,8 +16,9 @@ def cd(path):
 
 def test_qmagic_notebook():
     path = os.path.dirname(inspect.getfile(qmagic))
-    with open(os.path.join(path, 'qmagic.ipynb')) as f:
-        notebook = read(f, 'json')
-    r = NotebookRunner(notebook)
     with cd(path):
+        with open('qmagic.ipynb') as f:
+            notebook = read(f, 'json')
+        r = NotebookRunner(notebook)
+        assert os.getcwd() == path
         r.run_notebook()
