39bbdb78335a1eb285d7b52ad00558358c40ac99
Phillip Cloud <cpcloud@gmail.com>
2014-11-04 17:58:59 -0500

    Get recipe going for mac

--- recipes/q/build.sh
+++ recipes/q/build.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env sh
 
 
-SCRIPTS=$PREFIX/bin
-rm -rf $SCRIPTS/{q,Q}
-mkdir -p $SCRIPTS/Q
-cp -rf $SRC_DIR/* $SCRIPTS/Q
-rm -rf $SCRIPTS/q
-echo '#!/usr/bin/env sh' > $SCRIPTS/q
-echo 'export QHOME=$ANACONDA_ENVS/$CONDA_DEFAULT_ENV/bin/Q' >> $SCRIPTS/q
-echo '$ANACONDA_ENVS/$CONDA_DEFAULT_ENV/bin/Q/m32/q' >> $SCRIPTS/q
-chmod +x $SCRIPTS/q
+qlib=$PREFIX/lib/q
+mkdir -p $qlib
+mkdir -p $PREFIX/bin
+cp -rf $SRC_DIR/* $qlib
+
+
+[ `uname` = "Darwin" ] && arch=m || arch=l
+
+touch $PREFIX/bin/q
+chmod +x $PREFIX/bin/q
+
+cat <<EOF > $PREFIX/bin/q
+#!/usr/bin/env sh
+export QHOME=/opt/anaconda1anaconda2anaconda3/lib/q
+
+qpath=\$QHOME/${arch}32/q
+if [ ! -x "\$qpath" ]; then
+    chmod +x "\$qpath"
+fi
+
+\$qpath
+
+EOF
