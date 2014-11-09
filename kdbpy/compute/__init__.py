0a666205d2782bd45e7b43a0a521efc034b014c0
Phillip Cloud <cpcloud@gmail.com>
2014-11-09 13:09:26 -0500

    Clean up space a bit

--- kdbpy/compute/__init__.py
+++ kdbpy/compute/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import
 from multipledispatch import halt_ordering, restart_ordering
+
 halt_ordering()
 from .qext import *
 from .core import *
 restart_ordering()
+
 from .qtable import QTable, tables
 from . import q
