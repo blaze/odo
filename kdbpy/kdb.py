9ee4f6a78542889e64c50730271befe9cc663707
Phillip Cloud <cpcloud@gmail.com>
2014-11-09 10:54:53 -0500

    Keep whitespace for doctest

--- kdbpy/kdb.py
+++ kdbpy/kdb.py
@@ -253,7 +253,7 @@ class KQ(object):
         't'
         >>> t
            name  amount
-        id
+        id             
         1     a       1
         2     b       2
         3     c       3
