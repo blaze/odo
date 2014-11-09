193d04120944e3e73bc41e945f10166715f3c180
Phillip Cloud <cpcloud@gmail.com>
2014-11-09 15:06:18 -0500

    Test our keyed table keys finder

--- kdbpy/compute/tests/test_qtable.py
+++ kdbpy/compute/tests/test_qtable.py
@@ -1,6 +1,6 @@
 import pytest
 pytest.importorskip('blaze')
-from kdbpy.compute.qtable import tables
+from kdbpy.compute.qtable import tables, iskeyed, keys
 
 
 def test_columns(q, rq, sq):
@@ -19,7 +19,7 @@ QTable(tablename='t',
 def test_tables(kdb, rt, st, t):
     d = tables(kdb)
     keys = set(d.keys())
-    assert keys == set(['rt', 'st', 't'])
+    assert keys == set(['kt', 'rt', 'st', 't'])
     for s in rt, st, t:
         assert s.isidentical(d[s._name])
 
@@ -32,3 +32,23 @@ def test_tablename(q, rq, sq):
     assert q.tablename == 't'
     assert rq.tablename == 'rt'
     assert sq.tablename == 'st'
+
+
+def test_iskeyed(rq, sq, q):
+    assert iskeyed(rq)
+    assert iskeyed(sq)
+    assert not iskeyed(q)
+
+
+def test_keys(ktq, rq, sq, q):
+    rk = keys(rq)
+    assert rk == ['name']
+
+    sk = keys(sq)
+    assert sk == ['name']
+
+    qk = keys(q)
+    assert qk == []
+
+    kqk = keys(ktq)
+    assert kqk == ['house', 'id']
