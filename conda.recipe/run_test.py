447363af0992956b32cd7d97ce2037acd095eb1d
Phillip Cloud <cpcloud@gmail.com>
2014-11-13 15:59:19 -0500

    Run directly

--- conda.recipe/run_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-
-if __name__ == '__main__':
-    import os
-    import kdbpy
-    from kdbpy.kdb import which
-    assert which('q.bat' if os.name == 'nt' else 'q')
-    kdbpy.test()
