c2e716df3692249afe6363c9d63aa7518c469685
Phillip Cloud <cpcloud@gmail.com>
2014-11-24 16:51:55 -0500

    windows try 1

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,6 +1,11 @@
-
 @echo off
 
 SET BLD_DIR=%CD%
 cd /D "%RECIPE_DIR%\.."
+FOR /F "delims=" %%i IN ('git describe --tags') DO set BLAZE_VERSION=%%i
+echo %BLAZE_VERSION% | %PYTHON% %SRC_DIR%\conda.recipe\version.py > tmp
+set result=<tmp
+
+del tmp
+echo %result%>__conda_version__.txt
 "%PYTHON%" setup.py install
