635664ec134a157a4ae19741494d60ef80085507
Phillip Cloud <cpcloud@gmail.com>
2014-11-23 21:28:53 -0500

    Remove bar crud

--- kdbpy/compute/qext.py
+++ kdbpy/compute/qext.py
@@ -1,16 +1,8 @@
 from .expr import bar
 from .. import q
 from .core import dispatch
-import blaze as bz
 
 
 @dispatch(bar, q.Expr)
 def compute_up(expr, data, **kwargs):
-    child = compute_up(expr._child, data, **kwargs)
-    return q.List('xbar', expr.n, child)
-
-
-@dispatch(bar, q.Expr, dict)
-def post_compute(expr, data, d):
-    f = post_compute.dispatch(bz.expr.Field, type(data), type(d))
-    return f(expr, data, d)
+    return q.List('xbar', expr.n, data)
