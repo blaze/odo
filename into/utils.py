888f53a1bde1fcf408e9640baa1fd224d1aa1848
Matthew Rocklin <mrocklin@gmail.com>
2014-12-07 16:46:19 -0800

    tmpfile

--- into/utils.py
+++ into/utils.py
@@ -1,3 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+from contextlib import contextmanager
+import tempfile
+import os
+
 
 def expand_tuples(L):
     """
@@ -17,3 +23,20 @@ def expand_tuples(L):
         rest = expand_tuples(L[1:])
         return [(item,) + t for t in rest for item in L[0]]
 
+@contextmanager
+def tmpfile(extension=''):
+    extension = '.' + extension.lstrip('.')
+    handle, filename = tempfile.mkstemp(extension)
+
+    yield filename
+
+    try:
+        if os.path.exists(filename):
+            os.remove(filename)
+    except OSError:  # Sometimes Windows can't close files
+        if os.name == 'nt':
+            os.close(handle)
+            try:
+                os.remove(filename)
+            except OSError:  # finally give up
+                pass
