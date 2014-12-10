4fd70591a40b2b35f6068e5c277ec85b01570c76
Matthew Rocklin <mrocklin@gmail.com>
2014-12-09 17:22:50 -0800

    pandas coerce datetimes

--- into/backends/pandas.py
+++ into/backends/pandas.py
@@ -18,3 +18,33 @@ def discover_dataframe(df):
 @discover.register(pd.Series)
 def discover_series(s):
     return len(s) * datashape.CType.from_numpy_dtype(s.dtype)
+
+
+def coerce_datetimes(df):
+    """ Make object columns into datetimes if possible
+
+    Warning: this operates inplace.
+
+    Example
+    -------
+
+    >>> df = pd.DataFrame({'dt': ['2014-01-01'], 'name': ['Alice']})
+    >>> df.dtypes  # note that these are strings/object
+    dt      object
+    name    object
+    dtype: object
+
+    >>> df2 = coerce_datetimes(df)
+    >>> df2
+              dt   name
+    0 2014-01-01  Alice
+
+    >>> df2.dtypes  # note that only the datetime-looking-one was transformed
+    dt      datetime64[ns]
+    name            object
+    dtype: object
+    """
+    df2 = df.select_dtypes(include=['object']).apply(pd.to_datetime)
+    for c in df2.columns:
+        df[c] = df2[c]
+    return df
