764a7fe9fbd8715dbc21659dc67dac4592b01d28
Matthew Rocklin <mrocklin@gmail.com>
2014-12-15 15:20:59 -0800

    move all dot code within function

--- into/dot.py
+++ into/dot.py
@@ -10,32 +10,31 @@ from .append import append
 from .utils import cls_name
 
 
-# Edges from Convert
-dg = nx.DiGraph()
-for a, b in convert.graph.edges():
-    cost = convert.graph.edge[a][b]['cost']
-    dg.add_edge(cls_name(a), cls_name(b),
-                cost=cost,
-                penwidth=max(log(1./(cost + 0.06)), 1))
-
-
-# Edges from Append
-for a, b in append.funcs:
-    if b is not object and a != b:
-        dg.add_edge(cls_name(b), cls_name(a), color='blue')
-
-
-# Color edges
-for n in convert.graph.nodes() + list(pluck(0, append.funcs)):
-    if issubclass(n, tuple(ooc_types)):
-        dg.node[cls_name(n)]['color'] = 'red'
-
-# Convert to pydot
-p = nx.to_pydot(dg)
-
-
 # Write out as dot
 def dot_graph(filename='conversions'):
+    # Edges from Convert
+    dg = nx.DiGraph()
+    for a, b in convert.graph.edges():
+        cost = convert.graph.edge[a][b]['cost']
+        dg.add_edge(cls_name(a), cls_name(b),
+                    cost=cost,
+                    penwidth=max(log(1./(cost + 0.06)), 1))
+
+
+    # Edges from Append
+    for a, b in append.funcs:
+        if b is not object and a != b:
+            dg.add_edge(cls_name(b), cls_name(a), color='blue')
+
+
+    # Color edges
+    for n in convert.graph.nodes() + list(pluck(0, append.funcs)):
+        if issubclass(n, tuple(ooc_types)):
+            dg.node[cls_name(n)]['color'] = 'red'
+
+    # Convert to pydot
+    p = nx.to_pydot(dg)
+
     with open(filename + '.dot', 'w') as f:
         f.write(p.to_string())
 
