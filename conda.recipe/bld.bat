e2e3fad91a48328850193b0eb45b0f190ba2515e
Matthew Rocklin <mrocklin@gmail.com>
2014-12-15 13:21:28 -0800

    add conda/binstar recipes

---
+++ conda.recipe/bld.bat
@@ -0,0 +1,7 @@
+@echo off
+
+SET BLD_DIR=%CD%
+cd /D "%RECIPE_DIR%\.."
+FOR /F "delims=" %%i IN ('git describe --tags') DO set INTO_VERSION=%%i
+echo.%INTO_VERSION% | %PYTHON% .\conda.recipe\version.py > %SRC_DIR%\__conda_version__.txt
+%PYTHON% setup.py --quiet install
