26298cb3b459b4c0644864da0ef72a61840161f3
Matthew Rocklin <mrocklin@gmail.com>
2014-12-16 14:19:29 -0800

    update conda.recipe

--- conda.recipe/bld.bat
+++ conda.recipe/bld.bat
@@ -1,8 +1,7 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
+@echo off
 
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+SET BLD_DIR=%CD%
+cd /D "%RECIPE_DIR%\.."
+FOR /F "delims=" %%i IN ('git describe --tags') DO set INTO_VERSION=%%i
+echo.%INTO_VERSION% | %PYTHON% .\conda.recipe\version.py > %SRC_DIR%\__conda_version__.txt
+%PYTHON% setup.py --quiet install
--- conda.recipe/build.sh
+++ conda.recipe/build.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+BLD_DIR=`pwd`
+
+# Recipe and source are stored together
+SRC_DIR=$RECIPE_DIR/..
+pushd $SRC_DIR
+
+# X.X.X.dev builds
+u_version=`git describe --tags | $PYTHON $SRC_DIR/conda.recipe/version.py`
+
+echo $u_version > __conda_version__.txt
+
+cp __conda_version__.txt $BLD_DIR
+
 $PYTHON setup.py install
+popd
 
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
