5e4d6027aa194c697bb12e27cf37045beb039e32
Matthew Rocklin <mrocklin@gmail.com>
2014-12-15 17:04:55 -0800

    fool around with setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from os.path import exists
 from setuptools import setup
 
 setup(name='into',
-      version='0.1.1',
+      version='0.1.2',
       description='Data migration utilities',
       url='http://github.com/ContinuumIO/into/',
       author='Matthew Rocklin',
       author_email='mrocklin@continuum.io',
       license='BSD',
       keywords='into data conversion hdf5 sql blaze',
-      packages=['into', 'into/backends'],
+      packages=['into', 'into.backends'],
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
