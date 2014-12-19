678dba7413097584612b111bae2a22f31f4ed409
Phillip Cloud <cpcloud@gmail.com>
2014-12-19 18:13:52 -0500

    Use db instead of passing both fixtures around

--- kdbpy/compute/tests/test_partitioned.py
+++ kdbpy/compute/tests/test_partitioned.py
@@ -11,82 +11,79 @@ from kdbpy.tests import assert_series_equal
 
 
 @pytest.fixture(scope='module')
-def quote(rstring, kdbpar):
-    return Data(rstring + '/start/db::quote')
+def db(kdbpar):
+    return Data(kdbpar)
 
 
-@pytest.fixture(scope='module')
-def trade(rstring, kdbpar):
-    return Data(rstring + '/start/db::trade')
+def test_is_partitioned(db):
+    assert is_partitioned(db.trade)
+    assert is_partitioned(db.quote)
+    assert not is_partitioned(db.nbbo_t)
 
 
-def test_is_partitioned(trade):
-    assert is_partitioned(trade)
-
-
-def test_projection(trade, kdbpar):
-    expr = trade[['price', 'sym']]
+def test_projection(db):
+    expr = db.trade[['price', 'sym']]
     result = compute(expr)
-    expected = kdbpar.eval('select price, sym from trade')
+    expected = db.data.eval('select price, sym from trade')
     tm.assert_frame_equal(result, expected)
 
 
-def test_head(trade, kdbpar):
-    expr = trade.head()
+def test_head(db):
+    expr = db.trade.head()
     result = compute(expr)
-    expected = kdbpar.eval('.Q.ind[trade; til 10]')
+    expected = db.data.eval('.Q.ind[trade; til 10]')
     tm.assert_frame_equal(result, expected)
 
 
-def test_repr(trade):
-    assert repr(trade)
+def test_repr(db):
+    assert repr(db.trade)
 
 
-def test_field(trade, kdbpar):
-    expr = trade.price
+def test_field(db):
+    expr = db.trade.price
     result = compute(expr)
-    expected = kdbpar.eval('select price from trade').squeeze()
+    expected = db.data.eval('select price from trade').squeeze()
     assert_series_equal(result, expected)
 
 
-def test_simple_by(trade, kdbpar):
-    expr = by(trade.sym, w=trade.price.mean())
+def test_simple_by(db):
+    expr = by(db.trade.sym, w=db.trade.price.mean())
     result = compute(expr)
     query = 'select w: avg price by sym from trade'
-    expected = kdbpar.eval(query).reset_index()
+    expected = db.data.eval(query).reset_index()
     tm.assert_frame_equal(result, expected)
 
 
-def test_selection(trade, kdbpar):
-    expr = trade[trade.sym == 'AAPL']
+def test_selection(db):
+    expr = db.trade[db.trade.sym == 'AAPL']
     result = compute(expr)
-    expected = kdbpar.eval('select from trade where sym = `AAPL')
+    expected = db.data.eval('select from trade where sym = `AAPL')
     tm.assert_frame_equal(result, expected)
 
 
-def test_partitioned_nrows_on_virtual_column(quote):
-    assert compute(quote.nrows) == compute(quote.date.nrows)
+def test_partitioned_nrows_on_virtual_column(db):
+    assert compute(db.quote.nrows) == compute(db.quote.date.nrows)
 
 
 @pytest.mark.xfail(raises=QException)
-def test_field_head(trade, kdbpar):
-    result = compute(trade.price.head(5))
+def test_field_head(db):
+    result = compute(db.trade.price.head(5))
     query = 'exec price from select price from .Q.ind[trade; til 5]'
-    expected = kdbpar.eval(query)
+    expected = db.data.eval(query)
     assert_series_equal(result, expected, check_exact=True)
 
 
 @pytest.mark.xfail(raises=QException,
                    reason='field expressions not working')
-def test_simple_arithmetic(trade):
-    expr = trade.price + 1 * 2
+def test_simple_arithmetic(db):
+    expr = db.trade.price + 1 * 2
     result = compute(expr)
-    expected = trade.eval('exec price from select ((price + 1) * 2) from trade')
+    expected = db.data.eval('select ((price + 1) * 2) from trade').squeeze()
     assert_series_equal(result, expected)
 
 
 @pytest.mark.xfail(raises=QException, reason='not yet implemented')
-def test_nunique(trade, kdbpar):
-    expr = trade.sym.nunique()
-    assert compute(expr) == kdbpar.eval('count distinct exec sym from '
-                                        'select sym from trade')
+def test_nunique(db):
+    expr = db.trade.sym.nunique()
+    assert compute(expr) == db.data.eval('count distinct exec sym from '
+                                         'select sym from trade')
