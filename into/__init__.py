09eb689755f49f1a52c4d49efa0eb7d2d6df487f
Phillip Cloud <cpcloud@gmail.com>
2015-01-19 10:40:04 -0500

    Only catch ImportError in main __init__.py

--- into/__init__.py
+++ into/__init__.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
+__all__ = """convert append resource into drop chunks Chunks discover dshape
+             CSV JSON JSONLines S3""".split()
+
 from multipledispatch import halt_ordering, restart_ordering
 
-halt_ordering() # Turn off multipledispatch ordering
+halt_ordering()  # Turn off multipledispatch ordering
 
 from .convert import convert
 from .append import append
@@ -11,7 +14,6 @@ from .into import into
 from .drop import drop
 from .chunks import chunks, Chunks
 from datashape import discover, dshape
-from collections import Iterator
 import numpy as np
 
 try:
@@ -20,47 +22,47 @@ except:
     pass
 try:
      from .backends.pandas import pd
-except:
+except ImportError:
     pass
 try:
      from .backends.bcolz import bcolz
-except:
+except ImportError:
     pass
 try:
      from .backends.h5py import h5py
-except:
+except ImportError:
     pass
 try:
      from .backends.hdfstore import HDFStore
-except:
+except ImportError:
     pass
 try:
      from .backends.pytables import tables
-except:
+except ImportError:
     pass
 try:
      from .backends.dynd import nd
-except:
+except ImportError:
     pass
 try:
      from .backends import sql
-except:
+except ImportError:
     pass
 try:
      from .backends import mongo
-except:
+except ImportError:
     pass
 try:
      from .backends.csv import CSV
-except:
+except ImportError:
     pass
 try:
      from .backends.json import JSON, JSONLines
-except:
+except ImportError:
     pass
 try:
      from .backends import sql_csv
-except:
+except ImportError:
     pass
 try:
     from .backends.aws import S3
@@ -68,6 +70,6 @@ except ImportError:
     pass
 
 
-restart_ordering() # Restart multipledispatch ordering and do ordering
+restart_ordering()  # Restart multipledispatch ordering and do ordering
 
 __version__ = '0.1.3'
--- into/backends/aws.py
+++ into/backends/aws.py
@@ -1,10 +1,13 @@
 from __future__ import print_function, division, absolute_import
 
-from into import discover, CSV, resource, convert
-import pandas as pd
-from ..utils import cls_name
+import os
+import tempfile
+
 from toolz import memoize
-from datashape import var
+import boto
+
+from into import discover, CSV, resource, convert
+from ..utils import mkdir_p
 
 
 class _S3(object):
@@ -15,9 +18,12 @@ class _S3(object):
 def S3(cls):
     """Parametrized S3 bucket Class
 
-    Notes
-    -----
-    * Shamelessly copied from ``into.chunks``
+    Examples
+    --------
+    >>> import sqlalchemy
+    >>> S3(CSV)
+    S3(CSV)
+    >>> S3(sqlalchemy.Table)
     """
     return type('S3(%s)' % cls.__name__, (cls, _S3), {'subtype': cls})
 
@@ -28,10 +34,151 @@ def resource_s3_csv(uri):
 
 
 @convert.register(CSV, S3(CSV))
-def convert_s3_to_csv(bucket, **kwargs):
-    return CSV(bucket.path, **kwargs)
+def convert_s3_to_csv(csv, **kwargs):
+    """Download an S3 CSV to a local CSV file."""
+    path = csv.path
+    dirname, basename = os.path.split(path)
+    dirname = remove_substring(dirname)
+
+    mkdir_p(dirname)
+
+    conn_kwargs = dict(aws_access_key_id=kwargs.pop('aws_access_key_id', None),
+                       aws_secret_access_key=(
+                                    kwargs.pop('aws_secret_access_key', None)))
+    conn = get_s3_connection(**conn_kwargs)
+    bucket = conn.get_bucket(dirname)
+    key = bucket.get_key(basename)
+    filename = os.path.join(dirname, basename)
+    key.get_contents_to_filename(filename)
+    return CSV(filename, **kwargs)
+
+
+def get_part(bucket, key, conn=None, start=0, stop=8192,
+             aws_access_key_id=None, aws_secret_access_key=None, **kwargs):
+    """Get bytes from `start` to `stop` of `key` from the S3 bucket `bucket`.
+
+    Parameters
+    ----------
+    bucket : str
+        Bucket name
+    key : str
+        File name
+    conn : Connection, optional, default ``None``
+        S3 connection
+    start : int, optional, default ``0``
+        Byte to start at, zero based
+    stop : int, optional, default ``8192``
+        Byte to stop at, zero based
+    aws_access_key_id : str, optional, default ``None``
+        Access key id. Will be taken from the environment if ``None``.
+    aws_secret_access_key : str, optional, default ``None``
+        Secret access key. Will be taken from the environment if ``None``.
+    kwargs : dict
+        Keyword arguments to pass to the CSV constructor
+
+    Returns
+    -------
+    csv : CSV
+        The ``stop - start`` bytes of the S3 bucket named
+        ``s3://<bucket>/<key>`` as a CSV file.
+
+    Examples
+    --------
+    >>> csv = get_part(bucket='nyqpug', key='tips.csv')
+    >>> print(discover(csv))
+    var * {
+      total_bill: float64,
+      tip: float64,
+      sex: string,
+      smoker: string,
+      day: string,
+      time: string,
+      size: int64
+      }
+    """
+    assert stop - start > 0
+
+    if conn is None:
+        conn = get_s3_connection(aws_access_key_id=aws_access_key_id,
+                                 aws_secret_access_key=aws_secret_access_key)
+
+    # TODO: is Range always accepted by S3?
+    # TODO: is Content-Type useful here?
+    resp = conn.make_request('GET',
+                             bucket=bucket,
+                             key=key,
+                             headers={'Range': 'bytes=%d-%d' % (start, stop)})
+    raw = resp.read()
+    raw = raw[:raw.rindex(b'\n')]
+    with tempfile.NamedTemporaryFile(mode='wb', delete=False) as f:
+        f.write(raw)
+    return CSV(f.name, **kwargs)
+
+
+@memoize
+def get_s3_connection(aws_access_key_id, aws_secret_access_key, anon=False):
+    if aws_access_key_id is None:
+        aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+
+    if aws_secret_access_key is None:
+        aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    # anon is False but we didn't provide any credentials so try anonymously
+    anon = (not anon and
+            aws_access_key_id is None and
+            aws_secret_access_key is None)
+    return boto.connect_s3(aws_access_key_id, aws_secret_access_key,
+                           anon=anon)
+
+
+@convert.register(S3(CSV), CSV)
+def convert_csv_to_s3(csv,
+                      bucket_name=None,
+                      aws_access_key_id=None,
+                      aws_secret_access_key=None,
+                      **kwargs):
+    assert bucket_name is not None, 'must provide a bucket name'
+    conn = get_s3_connection(aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key)
+    try:
+        bucket = conn.get_bucket(bucket_name)
+    except boto.exception.S3ResponseError:  # bucket doesn't exist so create it
+        bucket = conn.create_bucket(bucket_name)
+
+    key = boto.s3.key.Key(bucket)
+    key.key = os.path.basename(csv.path)
+
+    # TODO: checkout chunked uploads for huge files
+    key.set_contents_from_filename(csv.path)
+    return S3(CSV('s3://%s/%s' % (bucket.name, key.key)))
+
+
+def remove_substring(s, substring='s3://'):
+    """Remove `substring` from `s`.
+
+    Parameters
+    ----------
+    s, substring : str
+
+    Returns
+    -------
+    stripped : str
+        `s` without `substring`
+
+    Examples
+    --------
+    >>> s = 's3://nyqpug/tips.csv'
+    >>> remove_substring(s)
+    'nyqpug/tips.csv'
+    >>> s = 'http://nyqpug/tips.csv'
+    >>> remove_substring(s, substring='foo/bar')
+    'http://nyqpug/tips.csv'
+    """
+    return s.replace(substring, '')
 
 
 @discover.register(S3(CSV))
 def discover_s3_csv(c, **kwargs):
-    return var * discover.dispatch(CSV)(c).subshape[0]
+    path = remove_substring(c.path)
+    bucket, key = os.path.split(path)
+    return discover(get_part(bucket=bucket, key=key, **kwargs))
