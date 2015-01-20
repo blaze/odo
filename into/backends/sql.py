f2e313abe7465c8dcb30a61ddd301309a366062f
Matthew Rocklin <mrocklin@gmail.com>
2015-01-20 08:40:23 -0800

    sql.Select -> Iterator/base
    Convert sql select statements into Python iterators, ints, floats, etc...

--- into/backends/sql.py
+++ into/backends/sql.py
@@ -4,9 +4,10 @@ import sqlalchemy as sa
 from itertools import chain
 from collections import Iterator
 from datashape import (DataShape, Record, Option, var, dshape)
-from datashape.predicates import isdimension, isrecord
+from datashape.predicates import isdimension, isrecord, isscalar
 from datashape import discover
 from datashape.dispatch import dispatch
+from datetime import datetime, date
 import datashape
 from toolz import partition_all, keyfilter, first
 
@@ -15,6 +16,9 @@ from ..convert import convert, ooc_types
 from ..append import append
 from ..resource import resource
 from ..chunks import Chunks
+
+base = (int, float, datetime, date, bool, str)
+
 
 # http://docs.sqlalchemy.org/en/latest/core/types.html
 
@@ -208,6 +212,32 @@ def sql_to_iterator(t, **kwargs):
         result = conn.execute(sa.sql.select([t]))
         for item in result:
             yield item
+
+
+@convert.register(Iterator, sa.sql.Select, cost=300.0)
+def select_to_iterator(sel, dshape=None, **kwargs):
+    engine = sel.bind  # TODO: get engine from select
+
+    with engine.connect() as conn:
+        result = conn.execute(sel)
+
+    if not dshape:
+        return result
+    if isscalar(dshape.measure):
+        return [x[0] for x in result]
+    return result
+
+
+@convert.register(base, sa.sql.Select, cost=300.0)
+def select_to_base(sel, dshape=None, **kwargs):
+    engine = sel.bind  # TODO: get engine from select
+
+    with engine.connect() as conn:
+        result = conn.execute(sel)
+
+    assert not dshape or isscalar(dshape)
+
+    return list(result)[0][0]
 
 
 @append.register(sa.Table, Iterator)
