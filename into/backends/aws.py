a486ec1b210e687d388c878c96be927ff006cbe8
Phillip Cloud <cpcloud@gmail.com>
2015-01-30 11:01:05 -0500

    Multipart uploading

--- into/backends/aws.py
+++ into/backends/aws.py
@@ -1,14 +1,20 @@
 from __future__ import print_function, division, absolute_import
 
 import os
+import tempfile
+import gzip
+from multiprocessing.pool import ThreadPool
+
 from contextlib import contextmanager
 
-from toolz import memoize
+from toolz import memoize, curry, pipe
+from toolz.curried import map
+
 import boto
 import pandas as pd
 
 from into import discover, CSV, resource, append, convert, drop
-from ..utils import tmpfile
+from ..utils import tmpfile, ext
 
 try:
     from urlparse import urlparse
@@ -121,7 +127,7 @@ def sample_s3_csv(data, length=8192):
     """
     # TODO: is Range always accepted by S3?
     # TODO: is Content-Type useful here?
-    headers = {'Range': 'bytes=0-%d' % length}#, 'Content-Type': 'text/csv'}
+    headers = {'Range': 'bytes=0-%d' % length}
     raw = data.object.get_contents_as_string(headers=headers)
 
     # this is generally cheap as we usually have a tiny amount of data
@@ -139,10 +145,118 @@ def sample_s3_csv(data, length=8192):
         yield fn
 
 
+def upload_part(mp, conn, pair):
+    """Upload part of a file to S3
+
+    Parameters
+    ----------
+    mp : MultiPartUpload
+    conn : boto.s3.connection.S3Connection
+    pair : tuple of int, string
+        The index and filename of the part of the file to upload
+    """
+    i, filename = pair
+    mp_i = boto.s3.multipart.MultiPartUpload(conn.lookup(mp.bucket_name))
+    mp_i.key_name = mp.key_name
+    mp_i.id = mp.id
+    with open(filename, mode='rb') as f:
+        mp_i.upload_part_from_file(f, i)
+    return filename
+
+
+_MIN_CHUNK_SIZE = 15 * 1 << 20
+
+
+@contextmanager
+def multipart_upload(key):
+    """Context manager to yield a multipart upload object for use in uploading
+    large files to S3
+
+    Parameters
+    ----------
+    key : boto.s3.key.Key
+        Key object to upload in multiple parts
+    """
+    mp = key.bucket.initiate_multipart_upload(key.name)
+    yield mp
+    mp.complete_upload()
+
+
+def pmap(f, iterable):
+    """Map `f` over `iterable` in parallel using a ``ThreadPool``.
+    """
+    p = ThreadPool()
+    try:
+        result = p.map(f, iterable)
+    finally:
+        p.terminate()
+    return result
+
+
+def write(triple, opener):
+    """Write a file using the input from `gentemp` using `opener` and return
+    its index and filename.
+
+    Parameters
+    ----------
+    triple : tuple of int, str, str
+        The first element is the index in the set of chunks of a file, the
+        second element is the path to write to, the third element is the data
+        to write.
+
+    Returns
+    -------
+    i, filename : int, str
+        File's index and filename. This is used to return the index and
+        filename after splitting files.
+    """
+    i, filename, data = triple
+    with opener(filename, mode='wb') as f:
+        f.write(data)
+    return i, filename
+
+
+def gentemp(it, suffix=None):
+    """Generate an index, a temporary file, and return data for each element
+    in `it
+
+    Parameters
+    ----------
+    it : iterable
+    suffix : str
+        Suffix to add to each temporary file's name
+    """
+    for i, data in enumerate(it, start=1):  # aws needs parts to start at 1
+        _, fn = tempfile.mkstemp(suffix=suffix)
+        yield i, fn, data
+
+
+def split(filename, nbytes=_MIN_CHUNK_SIZE, suffix=None, compression=None):
+    with open(filename, mode='rb') as f:
+        return pmap(curry(write, compression=compression),
+                    gentemp(iter(curry(f.read, nbytes), ''), suffix=suffix))
+
+
 @append.register(_S3, CSV)
-def append_csv_to_s3(s3, csv, **kwargs):
-    # TODO: checkout chunked uploads for huge files
-    s3.object.set_contents_from_filename(csv.path)
+def append_csv_to_s3(s3, csv,
+                     cutoff=100 * 1 << 20,
+                     chunksize=_MIN_CHUNK_SIZE,
+                     compression=None,
+                     **kwargs):
+    if os.path.getsize(csv.path) >= cutoff:  # defaults to 100MB
+        if chunksize < _MIN_CHUNK_SIZE:
+            raise ValueError('multipart upload with chunksize < %d are not '
+                             'allowed by AWS' % _MIN_CHUNK_SIZE)
+        opener = gzip.GzipFile if compression == 'gz' else open
+        with multipart_upload(s3.object) as mp:
+            pipe(csv.path,
+                 curry(split,
+                       suffix=os.extsep + ext(csv.path),
+                       opener=opener),
+                 curry(pmap, curry(upload_part, mp, s3.s3)),
+                 map(os.remove))
+    else:
+        s3.object.set_contents_from_filename(csv.path)
     return s3
 
 
