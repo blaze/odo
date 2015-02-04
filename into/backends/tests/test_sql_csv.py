d49e33d140f46789daccef8756b7860a90a977bd
Matthew Rocklin <mrocklin@gmail.com>
2015-02-04 09:35:56 -0800

    os-sensitive path in sql_csv tests

--- into/backends/tests/test_sql_csv.py
+++ into/backends/tests/test_sql_csv.py
@@ -2,6 +2,7 @@ from into.backends.sql_csv import *
 from into import resource, into
 import datashape
 from into.utils import tmpfile
+import os
 
 
 def normalize(s):
@@ -9,41 +10,43 @@ def normalize(s):
     return s2
 
 
-csv = CSV('myfile.csv', delimiter=',', has_header=True)
+fn = os.path.abspath('myfile.csv')
+
+csv = CSV(fn, delimiter=',', has_header=True)
 ds = datashape.dshape('var * {name: string, amount: int}')
 tbl = resource('sqlite:///:memory:::my_table', dshape=ds)
 
 
 def test_postgres_load():
-    assert normalize(copy_command('postgresql', tbl, csv)) == normalize(r"""
-    COPY my_table from 'myfile.csv'
+    assert normalize(copy_command('postgresql', tbl, csv)) == normalize("""
+    COPY my_table from '%s'
         (FORMAT csv,
          DELIMITER E',',
          NULL '',
          QUOTE '"',
-         ESCAPE '\',
+         ESCAPE '\\',
          HEADER True,
          ENCODING 'utf-8');
-    """)
+    """ % fn)
 
 
 def test_sqlite_load():
     assert normalize(copy_command('sqlite', tbl, csv)) == normalize("""
-     (echo '.mode csv'; echo '.import myfile.csv my_table';) | sqlite3 :memory:
-     """)
+     (echo '.mode csv'; echo '.import %s my_table';) | sqlite3 :memory:
+     """ % fn)
 
 
 def test_mysql_load():
-    assert normalize(copy_command('mysql', tbl, csv)) == normalize(r"""
-            LOAD DATA  INFILE 'myfile.csv'
+    assert normalize(copy_command('mysql', tbl, csv)) == normalize("""
+            LOAD DATA  INFILE '%s'
             INTO TABLE my_table
             CHARACTER SET utf-8
             FIELDS
                 TERMINATED BY ','
                 ENCLOSED BY '"'
-                ESCAPED BY '\'
-            LINES TERMINATED BY '\n\r'
-            IGNORE 1 LINES;""")
+                ESCAPED BY '\\'
+            LINES TERMINATED BY '\\n\\r'
+            IGNORE 1 LINES;""" % fn)
 
 
 def test_into_sqlite():
