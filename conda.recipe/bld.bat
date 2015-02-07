e4f1bb66f64c3a3901d54b69055e53848924e590
Matthew Rocklin <mrocklin@gmail.com>
2015-02-07 07:46:39 -0800

    simplify conda build script

--- conda.recipe/bld.bat
+++
@@ -1,5 +0,0 @@
-SET BLD_DIR=%CD%
-cd /D "%RECIPE_DIR%\.."
-FOR /F "delims=" %%i IN ('git describe --tags') DO set INTO_VERSION=%%i
-echo.%INTO_VERSION% | "%PYTHON%" .\conda.recipe\version.py > "%SRC_DIR%\__conda_version__.txt"
-"%PYTHON%" setup.py --quiet install
