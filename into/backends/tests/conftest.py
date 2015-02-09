b1c317daae7c49e7b9a0666a2bab1cd87249bff0
Phillip Cloud <cpcloud@gmail.com>
2015-02-08 20:43:16 -0500

    Move sqlctx to conftest.py

--- into/backends/tests/conftest.py
+++ into/backends/tests/conftest.py
@@ -5,3 +5,8 @@ import pytest
 def sc():
     pyspark = pytest.importorskip('pyspark')
     return pyspark.SparkContext('local', 'into-test')
+
+
+@pytest.fixture(scope='session')
+def sqlctx(sc):
+    return pytest.importorskip('pyspark.sql.SQLContext')(sc)
