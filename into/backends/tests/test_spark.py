ec2a18c8da029aadb7bc853c73dc6e1484ddac3b
Phillip Cloud <cpcloud@gmail.com>
2015-02-12 12:18:56 -0500

    Test discover on a vanilla RDD

--- into/backends/tests/test_spark.py
+++ into/backends/tests/test_spark.py
@@ -1,5 +1,5 @@
 import pytest
-from into import into
+from into import into, discover
 from pyspark import RDD
 
 
@@ -17,3 +17,7 @@ def test_spark_into(rdd):
     seq = [1, 2, 3]
     assert isinstance(into(rdd, seq), RDD)
     assert into([], into(rdd, seq)) == seq
+
+
+def test_discover_rdd(rdd):
+    assert discover(rdd).subshape[0] == discover(data).subshape[0]
