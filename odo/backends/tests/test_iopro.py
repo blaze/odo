f1149a2907dfe52d6b86a7971590911933cd8cce
Phillip Cloud <cpcloud@gmail.com>
2015-03-21 17:34:04 -0400

    Fail on postgresql

--- odo/backends/tests/test_iopro.py
+++ odo/backends/tests/test_iopro.py
@@ -5,7 +5,6 @@ import pytest
 sa = pytest.importorskip('sqlalchemy')
 iopro = pytest.importorskip('iopro')
 pytest.importorskip('pyodbc')
-pytest.importorskip('psycopg2')
 
 from odo import into, convert, drop
 from odo.core import path
@@ -185,6 +184,8 @@ def test_iopro_mysql(mysql):
     iopro_sqlselect_to_ndarray_example(mysql)
 
 
+@pytest.mark.xfail(not hasattr(sa.dialects.postgresql, 'pyodbc'),
+                   reason="There's no postgresql+pyodbc driver for SQLAlchemy")
 def test_iopro_postgresql(postgres):
     iopro_sqltable_to_ndarray_example(postgres)
     iopro_sqlselect_to_ndarray_example(postgres)
