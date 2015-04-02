50158dd9b9f9d8c457347fa198fab84b01bdb994
Phillip Cloud <cpcloud@gmail.com>
2015-04-02 13:46:31 -0400

    cleanup

--- odo/convert.py
+++ odo/convert.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import numpy as np
 import pandas as pd
 from datashape.predicates import isscalar
-from toolz import concat, curry, partition_all
+from toolz import concat, partition_all
 from collections import Iterator, Iterable
 import datashape
 from datashape import discover
@@ -74,8 +74,8 @@ def series_to_array(s, dshape=None, **kwargs):
 
     # don't lose precision of datetime64 more precise than microseconds
     if ((issubclass(sdtype.type, np.datetime64) and
-            np.datetime_data(sdtype)[0] in higher_precision_freqs)
-            or s.dtype == dtype):
+            np.datetime_data(sdtype)[0] in higher_precision_freqs) or
+            s.dtype == dtype):
         return values
     try:
         return values.astype(dtype)
@@ -87,10 +87,10 @@ def series_to_array(s, dshape=None, **kwargs):
 def numpy_to_list(x, **kwargs):
     dt = None
     if x.dtype == 'M8[ns]':
-        dt = 'M8[us]' # lose precision when going to Python datetime
+        dt = 'M8[us]'  # lose precision when going to Python datetime
     if x.dtype.fields and any(x.dtype[n] == 'M8[ns]' for n in x.dtype.names):
         dt = [(n, 'M8[us]' if x.dtype[n] == 'M8[ns]' else x.dtype[n])
-                for n in x.dtype.names]
+              for n in x.dtype.names]
     if dt:
         return x.astype(dt).tolist()
     else:
