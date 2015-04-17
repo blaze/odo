2bf9d941490e32a6d959124b2b3e3b3c0c93dece
Phillip Cloud <cpcloud@gmail.com>
2015-04-17 11:05:30 -0400

    Versioneer things

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ import os
 from fnmatch import fnmatch
 from setuptools import setup, find_packages
 
+import versioneer
+versioneer.VCS = 'git'
+versioneer.versionfile_source = os.path.join('odo', '_version.py')
+versioneer.versionfile_build = versioneer.versionfile_source
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'odo-'  # dirname like 'myproject-1.2.0'
+
 
 def find_data_files(where, exts):
     exts = tuple(exts)
@@ -19,8 +26,14 @@ package_data = [x.replace('odo' + os.sep, '') for x in
                 find_data_files('odo', exts)]
 
 
+def read(filename):
+    with open(filename, 'r') as f:
+        return f.read()
+
+
 setup(name='odo',
-      version='0.3.1',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='Data migration utilities',
       url='http://github.com/ContinuumIO/odo',
       author='Blaze development deam',
@@ -28,8 +41,9 @@ setup(name='odo',
       license='BSD',
       keywords='odo data conversion hdf5 sql blaze',
       packages=find_packages(),
-      install_requires=list(open('requirements.txt').read().strip().split('\n')),
-      long_description=(open('README.rst').read() if os.path.exists('README.rst')
+      install_requires=read('requirements.txt').strip().split('\n'),
+      long_description=(read('README.rst')
+                        if os.path.exists('README.rst')
                         else ''),
       package_data={'odo': package_data},
       zip_safe=False,
