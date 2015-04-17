121f9ddd3b824158143edb5b2277639e90e500fb
Phillip Cloud <cpcloud@gmail.com>
2015-04-17 19:17:50 -0400

    Fix hanging

--- odo/backends/sql.py
+++ odo/backends/sql.py
@@ -119,7 +119,11 @@ def batch(sel, chunksize=10000):
 
             for rows in iter_except(curry(result.fetchmany, size=chunksize),
                                     sa.exc.ResourceClosedError):
-                yield rows
+                if rows:
+                    yield rows
+                else:
+                    # sqlalchemy 1.0.0 allows usage of the iterator after close
+                    return
     terator = rowterator(sel)
     return next(terator), concat(terator)
 
