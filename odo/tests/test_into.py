88ab8842101fd5aa5171b684fa10f17d90bfb54d
Phillip Cloud <cpcloud@gmail.com>
2015-05-29 09:28:17 -0400

    Test unicode uris

--- odo/tests/test_into.py
+++ odo/tests/test_into.py
@@ -1,18 +1,21 @@
 from __future__ import absolute_import, division, print_function
 
+import pytest
+
 from odo.into import into
 from odo.utils import tmpfile, filetext
 from odo.backends.csv import CSV
+
 
 def test_into_convert():
     assert into(list, (1, 2, 3)) == [1, 2, 3]
 
 
 def test_into_append():
-    L = []
-    result = into(L, (1, 2, 3))
+    lst = []
+    result = into(lst, (1, 2, 3))
     assert result == [1, 2, 3]
-    assert result is L
+    assert result is lst
 
 
 def test_into_curry():
@@ -21,17 +24,19 @@ def test_into_curry():
     assert into(list)(data) == into(list, data)
 
 
-def test_into_double_string():
+@pytest.mark.parametrize('f', [lambda x: u'%s' % x, lambda x: x])
+def test_into_double_string(f):
     with filetext('alice,1\nbob,2', extension='.csv') as source:
         assert into(list, source) == [('alice', 1), ('bob', 2)]
 
         with tmpfile('.csv') as target:
-            csv = into(target, source)
+            csv = into(f(target), source)
             assert isinstance(csv, CSV)
-            with open(target) as f:
+            with open(target, 'rU') as f:
                 assert 'alice' in f.read()
 
 
-def test_into_string_on_right():
+@pytest.mark.parametrize('f', [lambda x: u'%s' % x, lambda x: x])
+def test_into_string_on_right(f):
     with filetext('alice,1\nbob,2', extension='.csv') as source:
-        assert into([], source) == [('alice', 1), ('bob', 2)]
+        assert into([], f(source)) == [('alice', 1), ('bob', 2)]
