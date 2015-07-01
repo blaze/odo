fee4efeb8443db303de6197e07db19900d7a4f4f
ywang007 <ywang@continuum.io>
2015-07-01 18:24:08 -0400

    pep8

--- odo/tests/test_utils.py
+++ odo/tests/test_utils.py
@@ -6,16 +6,18 @@ def test_ext():
     assert ext('apy') == ''
     assert ext('myfile.csv.gz') == 'gz'
 
+
 def test_iter_except():
-    s = set([1, 2, 3])   
+    s = set([1, 2, 3])
+
     def iterate():
         yield 1
         yield 2
         yield 4
-        
+
     elems = iterate()
     inset = iter_except(lambda: next(elems) in s, StopIteration)
-    assert list(inset) ==[True, True, False]
+    assert list(inset) == [True, True, False]
 
 
 def test_keywords():
@@ -24,12 +26,14 @@ def test_keywords():
 
 def test_gentemp():
     i, _, data = next(gentemp([[1, 2], [3, 4]], start=1))
-    assert i==1 and data==[1, 2]
+    assert i == 1 and data == [1, 2]
 
 
 def test_records_to_tuples():
-    seq = [{'i':1, 'f':1.0}, {'i':2, 'f':2.0}]
-    l = list(records_to_tuples('var * {i: int, f: float64}', seq))
-    assert l == [(1, 1.0), (2, 2.0)]
-    assert records_to_tuples('var * int', 'dummy') == 'dummy'
+    seq = [{'i': 1, 'f': 1.0}, {'i': 2, 'f': 2.0}]
+    lst = list(records_to_tuples('var * {i: int, f: float64}', seq))
+    assert lst == [(1, 1.0), (2, 2.0)]
 
+
+def test_records_to_tuples_mismatch_passthrough():
+    assert records_to_tuples('var * int', 'dummy') == 'dummy'
