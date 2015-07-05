20436bce375016bd612aa8c59184ffbf99eefc13
Phillip Cloud <cpcloud@gmail.com>
2015-07-05 10:38:29 -0400

    Add more tests

--- odo/tests/test_directory.py
+++ odo/tests/test_directory.py
@@ -5,7 +5,7 @@ import tempfile
 from contextlib import contextmanager
 import shutil
 from odo.backends.csv import CSV
-from odo.directory import Directory, discover, resource
+from odo.directory import Directory, discover, resource, _Directory
 from odo import odo
 from datashape import dshape
 from toolz import concat
@@ -15,6 +15,8 @@ import pandas as pd
 
 @contextmanager
 def csvs(n=3):
+    assert 0 < n <= 10, \
+        'number of files must be greater than 0 and less than or equal to 10'
     path = tempfile.mktemp()
     os.mkdir(path)
 
@@ -51,8 +53,38 @@ def test_resource_nonexistent_directory():
         resource(os.path.join('a', 'nonexistent', 'directory') + os.path.sep)
 
 
+@contextmanager
+def chdir(newdir):
+    curdir = os.getcwd()
+    os.chdir(newdir)
+
+    try:
+        yield os.getcwd()
+    finally:
+        os.chdir(curdir)
+
+
 def test_directory_of_csvs_to_frame():
     with csvs() as path:
         result = odo(odo(path, pd.DataFrame), set)
+        expected = odo(concat(odo(fn, list) for fn in resource(path)), set)
+    assert result == expected
+
+
+def test_directory_of_csvs_with_cd():
+    with csvs() as path:
+        with chdir(path):
+            result = odo(odo('./*.csv', pd.DataFrame), set)
+        expected = odo(concat(odo(fn, list) for fn in resource(path)), set)
+    assert result == expected
+
+
+def test_directory_of_csvs_with_non_star_glob():
+    with csvs() as path:
+        with chdir(path):
+            glob_pat = '%sfile_?.csv' % path
+            dir_of_csv = resource(glob_pat)
+            assert isinstance(dir_of_csv, _Directory)
+            result = odo(odo(dir_of_csv, pd.DataFrame), set)
         expected = odo(concat(odo(fn, list) for fn in resource(path)), set)
     assert result == expected
