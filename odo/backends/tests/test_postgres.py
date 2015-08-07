d42381d52b0aa4a469d0a15e688c0e7e3a21cac0
llllllllll <joe@quantopian.com>
2015-08-07 15:44:46 -0400

    ENH: Don't put latin1 char in string literal

--- odo/backends/tests/test_postgres.py
+++ odo/backends/tests/test_postgres.py
@@ -188,7 +188,7 @@ def test_different_encoding(url):
                         (u'1958.001.500223-6', 1, None, u'', 9610),
                         (u'1958.001.500233-9', 1, None, u'', 4703),
                         (u'1909.017.000018-3', 1, 30.0, u'sumaria', 899),
-                        (u"", 1, 0, u"®", 0)]
+                        (u'', 1, 0, b'\xae'.decode('latin1'), 0)]
             assert result == expected
         finally:
             drop(sql)
