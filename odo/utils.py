12d9270ab0955ce252bc24c6d1a83e340b9a4ffb
Kurt Smith <kwmsmith@gmail.com>
2016-01-06 12:45:30 -0600

    Remove doctests from utility decorator.

--- odo/utils.py
+++ odo/utils.py
@@ -410,29 +410,6 @@ def deprecated(replacement=None):
     """A decorator which can be used to mark functions as deprecated.
     replacement is a callable that will be called with the same args
     as the decorated function.
-    
-    >>> @deprecated()
-    ... def foo(x):
-    ...     return x
-    ...
-    >>> ret = foo(1)
-    DeprecationWarning: foo is deprecated
-    >>> ret
-    1
-    >>>
-    >>>
-    >>> def newfun(x):
-    ...     return 0
-    ...
-    >>> @deprecated(newfun)
-    ... def foo(x):
-    ...     return x
-    ...
-    >>> ret = foo(1)
-    DeprecationWarning: foo is deprecated; use newfun instead
-    >>> ret
-    0
-    >>>
     """
     def outer(fun):
         msg = "{} is deprecated".format(fun.__name__)
