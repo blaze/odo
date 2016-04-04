aeb3087777e57eea0abd3f13feaa4bbce599f413
Kurt Smith <kwmsmith@gmail.com>
2016-04-04 13:56:04 -0500

    Fix iter() call with HTTPResponse.read().

--- odo/backends/url.py
+++ odo/backends/url.py
@@ -131,13 +131,8 @@ def append_urlX_to_X(target, source, **kwargs):
     with closing(urlopen(source.url, timeout=kwargs.pop('timeout', None))) as r:
         chunk_size = 16 * source.chunk_size
         with open(target.path, 'wb') as fp:
-            for chunk in iter(curry(r.read, chunk_size), ''):
+            for chunk in iter(curry(r.read, chunk_size), b''):
                 fp.write(chunk)
-                # NOTE: `HTTPResponse.read` returns b'' indefinitely once it is
-                # finished, so we have to explicitly test for an empty `chunk`
-                # and break to exit this loop.
-                if not chunk:
-                    break
             return target
 
 
