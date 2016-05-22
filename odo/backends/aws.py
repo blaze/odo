05f95301f698160e792a196020660479f428c06e
Skipper Seabold <jsseabold@gmail.com>
2016-05-22 11:23:21 -0500

    Allow use of profile_name for s3

--- odo/backends/aws.py
+++ odo/backends/aws.py
@@ -27,8 +27,12 @@ from ..utils import tmpfile, ext, sample, filter_kwargs, copydoc
 @memoize
 def get_s3_connection(aws_access_key_id=None,
                       aws_secret_access_key=None,
-                      anon=False):
+                      anon=False, **kwargs):
     import boto
+
+    if 'profile_name' in kwargs:
+        return boto.connect_s3(profile_name=kwargs['profile_name'])
+
     cfg = boto.Config()
 
     if aws_access_key_id is None:
@@ -70,7 +74,8 @@ class _S3(object):
             self.s3 = s3
         else:
             self.s3 = get_s3_connection(aws_access_key_id=aws_access_key_id,
-                                        aws_secret_access_key=aws_secret_access_key)
+                                        aws_secret_access_key=aws_secret_access_key,
+                                        **kwargs)
         try:
             bucket = self.s3.get_bucket(self.bucket,
                                         **filter_kwargs(self.s3.get_bucket,
@@ -154,7 +159,7 @@ def resource_s3_csv(uri, **kwargs):
 
 @resource.register('s3://.*\*.csv(\.gz)?', priority=19)
 def resource_s3_csv_glob(uri, **kwargs):
-    con = get_s3_connection()
+    con = get_s3_connection(**kwargs)
     result = urlparse(uri)
     bucket = con.get_bucket(result.netloc, validate=False)
     key = result.path.lstrip('/')
