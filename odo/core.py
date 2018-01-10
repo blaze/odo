8a4904c54d5f4f8d498ffac57b634caee739fff0
ANALYTICS\OrengoM <michel.orengo@moodys.com>
2018-01-10 14:08:50 -0500

    Compatibility Networkx 2

--- odo/core.py
+++ odo/core.py
@@ -169,10 +169,14 @@ def path(graph, source, target, excluded_edges=None, ooc_types=ooc_types):
                                     if issubclass(n, oocs)])
     with without_edges(graph, excluded_edges) as g:
         pth = nx.shortest_path(g, source=source, target=target, weight='cost')
-        edge = graph.edge
+        #https: // github.com / blaze / odo / issues / 579
+        #https://github.com/blaze/odo/issues/588
+        #node can be retrieved from graph via dict-like access
+        #edge = graph.edge
 
         def path_part(src, tgt):
-            node = edge[src][tgt]
+            #node = edge[src][tgt]
+            node = graph[src][tgt]
             return PathPart(src, tgt, node['func'], node['cost'])
 
         return map(path_part, pth, pth[1:])
@@ -189,7 +193,10 @@ def without_edges(g, edges):
     edges = edges or []
     held = dict()
     for a, b in edges:
-        held[(a, b)] = g.edge[a][b]
+        #https: // github.com / blaze / odo / issues / 579
+        #https://github.com/blaze/odo/issues/588
+        #held[(a, b)] = g.edge[a][b]
+        held[(a, b)] = g[a][b]
         g.remove_edge(a, b)
 
     try:
